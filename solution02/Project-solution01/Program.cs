f36d96a9e128aeb6e9f38db2aad4f9fcb07640c5
“karim <moza.m@gmail.com>
2026-04-15 13:31:51 +0400

    task 1 added

---
+++ solution02/Project-solution01/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Project_solution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add
+      
+            
+            Console.WriteLine("Please Enter First Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+            //Subtraction
+
+            Console.WriteLine("Please Enter First Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+
+            //multiplay
+            Console.WriteLine("Please Enter First Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            //Division
+
+            Console.WriteLine("Please Enter First Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} / {num2} = {num1 / (double)num2}");
+
+
+
+
+
+         
+
+
+        }
+    }
+}
