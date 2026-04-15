5088a60c39b75bc5046e13a722b8fa7d76293397
“karim <moza.m@gmail.com>
2026-04-15 11:49:58 +0400

    If Condition Practice Tasks

---
+++ solution04/Project-solution01/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Project_solution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Please Enter Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //if (num > 0)
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("Negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+
+            //Console.WriteLine("Please Enter an integer number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if(num%2==0)
+            //{
+            //    Console.WriteLine("Even Number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd Number");
+            //}
+
+
+
+
+
+
+
+
+        }
+    }
+}
