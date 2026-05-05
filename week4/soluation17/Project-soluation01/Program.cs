044e84a63e431a241af07106087ee9b6eab10f28
“karim <moza.m@gmail.com>
2026-05-05 15:26:18 +0400

    constractors

---
+++ week4/soluation17/Project-soluation01/Program.cs
@@ -0,0 +1,126 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+
+    namespace MovieSystem
+    {
+        // Movie 
+        class Movie
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+
+            private int rating;
+            public int Rating
+            {
+                get { return rating; }
+                set
+                {
+                    if (value >= 1 && value <= 10)
+                        rating = value;
+                    else
+                        Console.WriteLine("Rating must be between 1 and 10");
+                }
+            }
+
+            // Constructor
+            public Movie(string title, string genre, int rating)
+            {
+                Title = title;
+                Genre = genre;
+                Rating = rating;
+            }
+        }
+
+    // User Class
+    class User
+    {
+        public string Name { get; set; }
+
+        private int watchCount;
+        public int WatchCount
+        {
+            get { return watchCount; }
+        }
+
+        private List<string> watchedMovies = new List<string>();
+
+        public User(string name)
+        {
+            Name = name;
+            watchCount = 0;
+        }
+
+
+        // WatchSession Class
+        class WatchSession
+        {
+            public string UserName { get; set; }
+            public string MovieTitle { get; set; }
+
+            public WatchSession(string userName, string movieTitle)
+            {
+                UserName = userName;
+                MovieTitle = movieTitle;
+            }
+        }
+
+        
+
+            // Watch Movie
+            public void WatchMovie(Movie movie)
+            {
+                watchCount++;
+                watchedMovies.Add(movie.Title);
+
+                Console.WriteLine($"{Name} is watching {movie.Title}");
+            }
+
+            // Rate Movie
+            public void RateMovie(Movie movie, int rate)
+            {
+                // Bonus: Prevent rating before watching
+                if (!watchedMovies.Contains(movie.Title))
+                {
+                    Console.WriteLine("You must watch the movie first!");
+                    return;
+                }
+
+                if (rate >= 1 && rate <= 10)
+                {
+                    movie.Rating = rate;
+                    Console.WriteLine($"{Name} rated {movie.Title} = {rate}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid rating! Must be 1-10");
+                }
+            }
+
+            // Bonus: Print watched movies
+            public void PrintWatchedMovies()
+            {
+                Console.WriteLine("Watched Movies");
+                foreach (var movie in watchedMovies)
+                {
+                    Console.WriteLine(movie);
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Movie m1 = new Movie("Inception", "Sci-Fi", 9);
+                User u1 = new User("Moza");
+
+                u1.WatchMovie(m1);
+                u1.RateMovie(m1, 10);
+
+                Console.WriteLine($"Watch Count: {u1.WatchCount}");
+
+                u1.PrintWatchedMovies();
+            }
+        }
+    }
